b729f250e3641996e8fce39abf3c875fd4ecef0f
Advaita <135300205+A-dvaita@users.noreply.github.com>
2025-05-28 16:40:13 +0530

    Add variables gridSize and sketchPad

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+let gridSize = 0;
+let sketchPad = document.querySelector('#sketchPad');
 // fetch value of range selector
 let slider = document.querySelector('#myRange');
 let output = document.querySelector('#value');
 output.textContent = slider.value;
 slider.addEventListener('input', (e) => {
     output.textContent = `${slider.value}`;
+    gridSize = Number(output.textContent);
 });
+// use slider value to make a grid of selected size
+
+function makeGrid(){
+    for(let i=0; i<gridSize; i++){
+        let div = document.createElement("div");
+        sketchPad.appendChild(div);
+        for(let j=0; j<gridSize; j++){
+            let div = document.createElement("div");
+            sketchPad.appendChild(div);
+        }
+    }
+}
+
+// Add functionality to set size of grid button
+
+// let setSz = document.querySelector('#setSize');
+// setSz.addEventListener('click', (e) => {
+//     makeGrid();
+// });
